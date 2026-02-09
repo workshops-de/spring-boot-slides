e6d5fbbd01d5220dc591e9b2d02536acaea195e4
tidusIO <github-action@actions-template-sync.noreply.github.com>
2026-02-09 04:55:45 +0000

    chore(template): merge template changes :up:
    Signed-off-by: tidusIO <github-action@actions-template-sync.noreply.github.com>

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,11 +1,18 @@
-import type { ShikiSetupReturn } from "@slidev/types";
-import { defineShikiSetup } from "@slidev/types";
+import { rendererRich, transformerTwoslash } from '@shikijs/twoslash';
+import type { ShikiSetupReturn } from '@slidev/types';
+import { defineShikiSetup } from '@slidev/types';
 
 export default defineShikiSetup((): ShikiSetupReturn => {
   return {
     themes: {
-      dark: "material-theme-ocean",
-      light: "light-plus",
+      dark: 'material-theme-ocean',
+      light: 'light-plus',
     },
+    transformers: [
+      transformerTwoslash({
+        explicitTrigger: true,
+        renderer: rendererRich(),
+      }),
+    ],
   };
 });
